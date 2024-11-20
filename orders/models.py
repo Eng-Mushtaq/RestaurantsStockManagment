a578beb024f9b36dc0c3bc128f445fc0792adcfd
Eng <mushtaqhejaira@gmail.com>
2024-11-20 09:08:09 +0300

    Connec with Database

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+# Categories Model
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+    
+
+ # Categories Model 
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    image = models.ImageField(upload_to='products/')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Category
 def signup_view(request):
     return render(request, 'signup.html')
 def login_view(request):
     return render(request, 'login.html')
 def home_view(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    return render(request, 'home.html', {'categories': categories})
 def bakeds_view(request):
     return render(request, 'bakeds.html')
 def contact_view(request):
